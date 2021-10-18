afe0f569432448049ca935cc1a6b608f1af300d3
syeduroojkamal <syeduroojkamal26@gmail.com>
2021-10-18 22:52:18 +0530

    Decryption added

--- Cyber_Lab/1/caesar_cipher.py
+++ Cyber_Lab/1/caesar_cipher.py
@@ -1,10 +1,21 @@
 def encrypt(plain_text, key):    
     return ''.join(chr(ord(letter) + key) for letter in plain_text) 
 
+def decrypt(plain_text, key):    
+    return ''.join(chr(ord(letter) - key) for letter in plain_text) 
+
 def main():
-    plain_text = input()
-    key = int(input())
-    print(encrypt(plain_text, key))
+    # Input
+    plain_text = input("Enter a Message : ")
+    key = int(input("Enter Secret : "))
+    
+    # Encryption
+    encrypted = encrypt(plain_text, key)
+    print(f'Encrypted : {encrypted}')
+    
+    # Decryption
+    decrypted = decrypt(encrypted, key)
+    print(f'Decrypted : {decrypted}')
 
 if __name__ == "__main__":
     main()
